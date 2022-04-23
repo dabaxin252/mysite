d82e6983cf8777d2551bb45adce99cba9ea7bf06
dabaxin252 <104248487+dabaxin252@users.noreply.github.com>
2022-04-23 16:04:46 +0900

    Delete Sneakers directory

--- Sneakers/js/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.humberger-btn');
-const nav = document.querySelector('.main-nav');
-
-btn.addEventListener('click', () => {
-    nav.classList.toggle('open-menu')
-});
-
-AOS.init();
